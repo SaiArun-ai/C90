d2b07ff908b96de714770b8245ef50e8a1ff26b8
SaiArun-ai <72151107+SaiArun-ai@users.noreply.github.com>
2021-02-10 20:27:41 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+p1n ;
+p2n ;
+function LogIn(){
+    p1n = document.getElementById("player1N").value;
+    p2n = document.getElementById("player2N").value;
+    localStorage.setItem("p1n",p1n);
+    localStorage.setItem("p2n",p2n);
+    window.location = "game.html";
+}
